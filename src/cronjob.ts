84aaa6946b2e7122aaa46c1717fc33587c3a0f31
Kamas Lau <kamaslau@outlook.com>
2022-01-01 03:31:43 +0800

    Changed 更新环境配置相关

--- src/cronjob.ts
+++ src/cronjob.ts
@@ -14,14 +14,12 @@ import { URLSearchParams } from 'url'
 const consolePrefix = '⏱ cron job: '
 
 // 读取传感器数据
-const sensor = (() => {
-  return {
-    id: process.env.SENSOR_ID ?? null,
-    model: process.env.SENSOR_MODEL ? Number(process.env.SENSOR_MODEL) : 11,
-    gpio: process.env.SENSOR_GPIO ? Number(process.env.SENSOR_GPIO): 4,
-    url: process.env.SENSOR_REPORT_URL ?? ''
-  }
-})();
+const sensor = {
+  id: process.env.SENSOR_ID ?? null,
+  model: process.env.SENSOR_MODEL ? Number(process.env.SENSOR_MODEL) : 11,
+  gpio: process.env.SENSOR_GPIO ? Number(process.env.SENSOR_GPIO) : 4,
+  url: process.env.SENSOR_REPORT_URL ?? ''
+}
 
 const reportSensor = async (): Promise<void> => {
   if (sensor.id === null) return
